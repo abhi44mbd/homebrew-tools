974c4b028c3027d308ecb99eebdaea0ba9ac0ad4
Abhishek Kumar <abhishek44.mbd@gmail.com>
2026-05-22 02:09:02 +0530

    ..

--- Formula/nukeport.rb
+++ Formula/nukeport.rb
@@ -2,7 +2,7 @@ class Nukeport < Formula
   desc "Fast and safe CLI tool to kill processes running on ports"
   homepage "https://github.com/abhi44mbd/nukeport"
   url "https://github.com/abhi44mbd/nukeport/releases/download/v0.1.1/nukeport-darwin-arm64.tar.gz"
-  version "0.1.0"
+  version "0.1.1"
   sha256 "3c9d063e7fec7838a3f2af59bb26abe87d1ea98d698aa074d1cf555bc31ba0a7"
 
   def install
